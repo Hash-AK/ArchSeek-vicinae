f0a6a14f58a8d32f3e010df865d5cea870d73a17
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2026-09-15 16:01:55 -0400

    learning how to add actions;adding Arch logo in /assets

--- src/archseek.tsx
+++ src/archseek.tsx
@@ -10,7 +10,16 @@ import {
 
 export default function ArchSeek() {
     return(
-        <List searchBarPlaceholder="Enter a search term to start"></List>
-    )
+        <List searchBarPlaceholder="Enter a search term to start">
+			<List.Item title="Open the Arch Wiki" icon="Arch_Linux_logo.svg" actions={
+				<ActionPanel>
+					<Action.CopyToClipboard title="Copy url to clipboard" content="https://wiki.archlinux.org/title/Main_page" />
+					<Action.OpenInBrowser title="Open in browser" url="https://wiki.archlinux.org/title/Main_page"/>
+				</ActionPanel>
+			}
+			/>
+			</List>
+	
+    );
 
 }
