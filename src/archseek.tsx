6e0771a20390c660eb4a4a362a666b6befaac647
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2026-09-15 20:15:01 -0400

    experimenting with new functions, to be able to pull pages

--- src/archseek.tsx
+++ src/archseek.tsx
@@ -8,7 +8,9 @@ import {
 	Color,
 } from "@vicinae/api";
 
-
+export function getWikiPage() : string {
+	return "Hello World"
+}
 export default function ArchSeek() {
     return(
         <List isShowingDetail searchBarPlaceholder="Enter a search term to start">
@@ -17,8 +19,9 @@ export default function ArchSeek() {
 					<Action.CopyToClipboard title="Copy url to clipboard" content="https://wiki.archlinux.org/title/Main_page" />
 					<Action.OpenInBrowser title="Open in browser" url="https://wiki.archlinux.org/title/Main_page"/>
 				</ActionPanel>
-			}
-			/>
+			} detail={
+			<List.Item.Detail markdown={("# The Arch Wiki \n" + getWikiPage())} />
+			} />
 			<List.EmptyView title="No Page found" description="Try to search something else." icon={{ source: "Arch_Linux_logo.svg", tintColor: Color.SecondaryText}} />
 			</List>
 	
